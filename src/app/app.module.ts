040c4f6122c73fd6d1fbc44c6794e5d499289c61
Lenin Torres <lenin.torres@actuaria-ec.actuaria.com.ec>
2018-12-01 13:19:55 -0500

    First push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DeviceComponent } from './device/device.component';
+import { DeviceGridComponent } from './device-grid/device-grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeviceComponent,
+    DeviceGridComponent
   ],
   imports: [
     BrowserModule
